ded32b09235b58f21e959b6d9cda5e94425bb103
Tsuyoshi Koike <airtk2000@gmail.com>
2023-02-23 10:19:48 +0900

    fix

--- src/server.js
+++ src/server.js
@@ -1,11 +1,37 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
+
+// CORS
+const cors = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+}
+
+// ignore request for FavIcon. so there is no error in browser
+const ignoreFavicon = (req, res, next) => {
+  if (req.originalUrl.includes('favicon.ico')) {
+    res.status(204).end()
+  }
+  next()
+}
 
 // fn to create express server
 const create = async () => {
   const app = express()
-  app.get('/', (req, res) => res.send('Hello World'))
+  // CORSを許可する
+  app.use(cors)
+  app.use(ignoreFavicon)
+
+  //app.get('/', (req, res) => res.send('Hello World'))
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/client.html'))
+  })
   return app
 }
 
